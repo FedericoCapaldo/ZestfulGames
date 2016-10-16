310fb04ebe1ac629a16558544e225be9cbd1f34b
FedericoKCL <fed.deployment@aol.co.uk>
2016-10-16 11:30:25 -0700

    added partials. and first game

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
-
+var partials = require('express-partials');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(partials());
 
 app.get('/', function (req, res) {
   res.render('homepage');
 });
 
 app.get('/*', function(req, res){
-  // return home
+  res.render('homepage');
 });
 
 
